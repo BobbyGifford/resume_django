36a7a5dffedd3fdb36ee76e614b1ffc1768f78f2
Bobby <bobby077joe@gmail.com>
2019-02-11 16:41:31 -0800

    added side note

--- pages/models.py
+++ pages/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Project(models.Model):
     rank = models.IntegerField(blank=True, default=0)
     title = models.CharField(max_length=200)
+    side_note = models.CharField(max_length=200, blank=True)
     main_photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     description = models.TextField()
     tech_used = models.CharField(max_length=300)
